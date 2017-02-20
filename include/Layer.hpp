d5d33bddc9aad6988a0753bb973f459b4166bbb4
Noh-hoon Lee <darklycos002@gmail.com>
2017-02-20 23:47:38 +1300

    Add back propagation and train functions

--- include/Layer.hpp
+++ include/Layer.hpp
@@ -16,43 +16,59 @@ namespace DeepLearning
     protected:
         PrimitiveLayer*     m_preLayer;
         PrimitiveLayer*     m_postLayer;
-        int     m_numInput;
-        int     m_numOutput;
-        rVec    m_input;
-        rVec    m_output;
-        double* m_outputPtr;    //// not implemented yet
-        rMat    m_weight;
-        rVec    m_bias;
-        rMat    m_wGrad;
-        rVec    m_bGrad;
-        E_functionType  m_errorFunctionType;
-        O_filterType    m_outputFilterType;
+        int                 m_numInput;
+        int                 m_numOutput;
+        rVec                m_input;
+        rVec                m_output;
+        double*             m_outputPtr;    //// not implemented yet
+        rMat                m_weights;
+        rVec                m_bias;
+        rMat                m_wGrad;
+        rVec                m_bGrad;
+        rVec                m_delta;
+        double              m_learningRate;
+        E_functionType      m_errorFunctionType;
+        O_filterType        m_outputFilterType;
 
     public:
         PrimitiveLayer()
             : m_preLayer(NULL), m_postLayer(NULL), m_numInput(0), m_numOutput(0),
-              m_input(), m_output(), m_outputPtr(NULL), m_weight(), m_bias(), m_wGrad(), m_bGrad(),
-              m_errorFunctionType(T_CEE), m_outputFilterType(T_SOFTMAX)
+              m_input(), m_output(), m_outputPtr(NULL), m_weights(), m_bias(),
+              m_wGrad(), m_bGrad(), m_delta(), m_learningRate(0.001),
+              m_errorFunctionType(T_MSE), m_outputFilterType(T_IDENTITY)
         {}
         PrimitiveLayer(int nInput, int nOutput)
             : m_preLayer(NULL), m_postLayer(NULL), m_numInput(nInput), m_numOutput(nOutput),
-              m_input(), m_output(), m_outputPtr(NULL), m_weight(), m_bias(), m_wGrad(), m_bGrad(),
-              m_errorFunctionType(T_CEE), m_outputFilterType(T_SOFTMAX)
+              m_input(), m_output(), m_outputPtr(NULL), m_weights(), m_bias(),
+              m_wGrad(), m_bGrad(), m_delta(), m_learningRate(0.001),
+              m_errorFunctionType(T_MSE), m_outputFilterType(T_IDENTITY)
         {
             init(nInput, nOutput);
         }
 
-        rMat& getWeight(void) { return m_weight; }
-        rVec& getInput(void) { return m_input; }
-        rVec& getOutput(void) { return m_output; }
-        rVec& getBias(void) { return m_bias; }
+        PrimitiveLayer*&    getPreLayer(void)  {   return  m_preLayer; }
+        PrimitiveLayer*&    getPostLayer(void)  {   return  m_postLayer; }
+        rMat&               getWeights(void) { return m_weights; }
+        rVec&               getInput(void) { return m_input; }
+        rVec&               getOutput(void) { return m_output; }
+        rVec&               getBias(void) { return m_bias; }
+        rVec&               getDelta(void) { return m_delta; }
+        double&             getLearningRate(void) { return m_learningRate; }
+
+        void connectPostLayer(PrimitiveLayer* postLayer) {
+            m_postLayer = postLayer;
+            m_postLayer->getPreLayer() = this;
+        }
 
         void setErrorFunction(E_functionType type)  { m_errorFunctionType = type; }
         void setOutputFilter(O_filterType type)     { m_outputFilterType = type; }
         void init(int nInput, int nOutput);
         virtual void feedForward(double* input, int N) = 0;
+        virtual void feedForward() = 0;
+        virtual void backPropagation(double* target, int N) = 0;
+        virtual void backPropagation() = 0;
+        virtual void updateWeights() = 0;
         virtual double loss(int N, double* x, double* target) = 0;
-        virtual void backPropagation(double* dout, int N) = 0;
     };
 
     void PrimitiveLayer::init(int nInput, int nOutput)
@@ -61,16 +77,17 @@ namespace DeepLearning
         m_numOutput = nOutput;
         m_input.resize(nInput);
         m_output.resize(nOutput);
-        m_weight.resize(nInput,nOutput);
+        m_weights.resize(nInput,nOutput);
         m_wGrad.resize(nInput,nOutput);
         m_bias.resize(nOutput);
         m_bGrad.resize(nInput,nOutput);
+        m_delta.resize(nOutput);
 
         randomGenerator rg;
         //// temporary weight initialization
-        int n_weight = m_weight.data().size();
+        int n_weight = m_weights.data().size();
         for(int i = 0; i < n_weight; ++i) {
-            m_weight.data()[i] = rg.boxMuller();
+            m_weights.data()[i] = rg.boxMuller();
         }
         for(int i = 0; i < m_numOutput; ++i)
             m_bias[i] = rg.boxMuller();
@@ -85,8 +102,11 @@ namespace DeepLearning
         Layer(int nInput, int nOutput) : PrimitiveLayer(nInput, nOutput) {}
 
         void feedForward(double* input, int N);
+        void feedForward();
+        void backPropagation(double* target, int N);
+        void backPropagation();
+        void updateWeights();
         double loss(int N, double *x, double *target);
-        void backPropagation(double* dout, int N);
     };
 
     template <typename AF>
@@ -96,10 +116,76 @@ namespace DeepLearning
         for(int i = 0; i < m_numOutput; ++i) {
             double output = 0.0;
             for(int j = 0; j < m_numInput; ++j) {
-                output += m_input[j] * m_weight[j][i];
+                output += m_input[j] * m_weights[j][i];
             }
             output = output + m_bias[i];
             m_output[i] = AF::getActivation(output);
+        }
+    }
+
+    template <typename AF>
+    void Layer<AF>::feedForward() {
+        if(m_preLayer == NULL) {
+            std::cerr<<"Wrong use of the feed forward"<<std::endl;
+            return;
+        }
+
+        m_input.assign(m_preLayer->getOutput().begin(), m_preLayer->getOutput().end());
+
+        for(int i = 0; i < m_numOutput; ++i) {
+            double output = 0.0;
+            for(int j = 0; j < m_numInput; ++j) {
+                output += m_input[j] * m_weights[j][i];
+            }
+            output = output + m_bias[i];
+            m_output[i] = AF::getActivation(output);
+        }
+    }
+
+
+    template <typename AF>
+    void Layer<AF>::backPropagation(double* target, int N)
+    {
+        if(m_postLayer == NULL) {
+            for(int i = 0; i < N; ++i)
+                m_delta[i] = target[i] - m_output[i];
+        } else {
+            backPropagation();
+        }
+    }
+
+    template <typename AF>
+    void Layer<AF>::backPropagation()
+    {
+        if(m_postLayer == NULL) {
+            std::cerr<<"Wrong use of the back propagation"<<std::endl;
+            return;
+        }
+        rVec& postLayerDelta = m_postLayer->getDelta();
+        rMat& postLayerWeights = m_postLayer->getWeights();
+        rVec& currentDelta = m_delta;
+
+        int numOutput = postLayerWeights.ncol();
+        int numInput  = postLayerWeights.nrow();
+
+        for(int j = 0; j < numInput; ++j) {
+            double delta = 0;
+            for(int i = 0; i < numOutput; ++i) {
+                delta += postLayerWeights[j][i] * postLayerDelta[i];
+            }
+            currentDelta[j] = delta;
+        }
+    }
+
+    template <typename AF>
+    void Layer<AF>::updateWeights()
+    {
+        for(int j = 0; j < m_numOutput; ++j) {
+            double delta = m_delta[j];
+            for(int i = 0; i < m_numInput; ++i) {
+                m_weights[i][j] += m_learningRate * delta * AF::getActivationGradient(m_input[i]) * m_input[i];
+            }
+            m_bias[j] += m_learningRate * delta * AF::getActivationGradient(1.0);
         }
     }
 
@@ -130,21 +216,6 @@ namespace DeepLearning
 
         return 0.0;
     }
-
-    template <typename AF>
-    void Layer<AF>::backPropagation(double* dout, int N)
-    {
-        for(int i = 0; i < m_numOutput; ++i) {
-            double output = 0.0;
-            for(int j = 0; j < m_numInput; ++j) {
-                output += m_input[j] * m_weight[j][i];
-            }
-            output = output + m_bias[i];
-            m_output[i] = AF::getActivation(output);
-        }
-
-    }
-
 
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include "include/LossFunctions.hpp"
 #include "include/GradientDescent.hpp"
 
-
+#include "mkl.h"
 
 using namespace std;
 using namespace DeepLearning;
@@ -16,167 +16,181 @@ double function2(int N, double* x) {
     return x[0] * x[0] + x[1] * x[1];
 }
 
+void multiLayerTrain()
+{
+    double input[2] = { 0.6, 0.9 };
+    double target[3] = { 1, 0.3, 0.6 };
+    std::cout<<std::endl<<"Test multilayer Train"<<std::endl<<std::endl;
+    DeepLearning::Layer<DeepLearning::Sigmoid> layer0;
+    DeepLearning::Layer<DeepLearning::Sigmoid> layer1;
+    DeepLearning::Layer<DeepLearning::Naive> layer2;
+
+    layer0.init(2,5);
+    layer1.init(5,7);
+    layer2.init(7,3);
+
+    layer0.connectPostLayer(&layer1);
+    layer1.connectPostLayer(&layer2);
+
+    layer0.getLearningRate() = 0.001;
+    layer1.getLearningRate() = 0.01;
+    layer2.getLearningRate() = 0.1;
+
+    std::cout<<"Epoch : "<<-1<<"\ttarget = "<<target[0]<<" "<<target[1]<<" "<<target[2]<<std::endl;
+
+    for(int i = 0; i < 100; ++i) {
+        layer0.feedForward(input,2);
+        layer1.feedForward();
+        layer2.feedForward();
+
+        layer2.backPropagation(target,3);
+        layer1.backPropagation();
+        layer0.backPropagation();
+
+        layer0.updateWeights();
+        layer1.updateWeights();
+        layer2.updateWeights();
+
+        rVec& output = layer2.getOutput();
+        if(i%10 == 0)
+            std::cout<<"Epoch : "<<i<<"\toutput = "<<output[0]<<" "<<output[1]<<" "<<output[2]<<" "<<layer2.loss(3,layer2.getOutput().data(),target)<<std::endl;
+    }
+    std::cout<<std::endl<<"Test multilayer Train Done"<<std::endl<<std::endl;
+
+}
+
+void matrixSolveRegressionTest()
+{
+    int num_training_Data = 100000;
+    double matrix[9];
+
+    matrix[0] = 3.0;    matrix[1] = 0.0;   matrix[2] = 0.0;
+    matrix[3] = 0.0;   matrix[4] = 3.0;    matrix[5] = 0.0;
+    matrix[6] = 0.0;    matrix[7] = 0.0;   matrix[8] = 3.0;
+
+    double input[3 * num_training_Data];    /// b
+    double target[3 * num_training_Data];   /// result
+
+    int ld = 3;
+    int ipiv[3];
+    int info;
+    int one = 1;
+
+    DGETRF(&ld,&ld,matrix,&ld,ipiv,&info);
+    std::cout<<"info = "<<info<<std::endl;
+
+    for(int i = 0; i < num_training_Data; ++i) {
+        double* rhs = input + i * 3;
+        double* x   = target + i * 3;
+        x[0] = rhs[0] = (double)rand() / RAND_MAX * 100;
+        x[1] = rhs[1] = (double)rand() / RAND_MAX * 100;
+        x[2] = rhs[2] = (double)rand() / RAND_MAX * 100;
+
+        DGETRS("N", &ld, &one, matrix, &ld, ipiv, x, &ld, &info);
+//            std::cout<<x[0]<<" "<<x[1]<<" "<<x[2]<<std::endl;
+    }
+
+    std::cout<<std::endl<<"Test matrix solve"<<std::endl<<std::endl;
+    DeepLearning::Layer<DeepLearning::Naive> layer0;
+    DeepLearning::Layer<DeepLearning::Naive> layer1;
+    DeepLearning::Layer<DeepLearning::Naive> layer2;
+    DeepLearning::Layer<DeepLearning::Naive> layer3;
+
+    layer0.init(3,3);
+    layer1.init(3,3);
+    layer2.init(3,3);
+    layer3.init(3,3);
+
+    layer0.connectPostLayer(&layer1);
+    layer1.connectPostLayer(&layer2);
+    layer2.connectPostLayer(&layer3);
+
+    layer0.getLearningRate() = 0.000000015;
+    layer1.getLearningRate() = 0.00000015;
+    layer2.getLearningRate() = 0.0000015;
+    layer3.getLearningRate() = 0.000015;
+
+    for(int i = 0; i < num_training_Data; ++i) {
+        layer0.feedForward(input + i * 3, 3);
+        layer1.feedForward();
+        layer2.feedForward();
+        layer3.feedForward();
+
+        layer3.backPropagation(target + i * 3, 3);
+        layer2.backPropagation();
+        layer1.backPropagation();
+        layer0.backPropagation();
+
+        layer0.updateWeights();
+        layer1.updateWeights();
+        layer2.updateWeights();
+        layer3.updateWeights();
+
+        rVec& output = layer3.getOutput();
+        if(i%1000 == 0)
+            std::cout<<"Epoch : "<<i<<"\t loss = "<<layer3.loss(3, layer3.getOutput().data(), target + i * 3)<<std::endl;
+    }
+    std::cout<<" Train Done"<<std::endl;
+    std::cout<<" validation"<<std::endl;
+
+    double validation_rhs[3] = {10, 15, 25};
+    double answer[3] = {10, 15, 25};
+
+    DGETRS("N", &ld, &one, matrix, &ld, ipiv, answer, &ld, &info);
+
+    std::cout<<"Answer = "<<answer[0]<<" "<<answer[1]<<" "<<answer[2]<<std::endl;
+
+    layer0.feedForward(validation_rhs, 3);
+    layer1.feedForward();
+    layer2.feedForward();
+    layer3.feedForward();
+
+    rVec& output = layer3.getOutput();
+    std::cout<<"output = "<<output[0]<<" "<<output[1]<<" "<<output[2]<<std::endl;
+    std::cout<<std::endl<<"Test matrix solve Done"<<std::endl<<std::endl;
+}
+
+void trainTest()
+{
+    double input[2] = { 0.6, 0.9 };
+    double target[3] = { 1, 0.3, 0.6 };
+    std::cout<<std::endl<<"Test train"<<std::endl<<std::endl;
+    DeepLearning::Layer<DeepLearning::Naive> layer;
+    layer.init(2,3);
+    layer.getLearningRate() = 0.1;
+
+    std::cout<<"Epoch : "<<0<<" target = "<<target[0]<<" "<<target[1]<<" "<<target[2]<<std::endl;
+
+    for(int i = 0; i < 100; ++i) {
+        layer.feedForward(input,2);
+        layer.backPropagation(target,3);
+        layer.updateWeights();
+
+        rVec& output = layer.getOutput();
+        if(i%10 == 0)
+        std::cout<<"Epoch : "<<i<<" output = "<<output[0]<<" "<<output[1]<<" "<<output[2]<<std::endl;
+    }
+    std::cout<<std::endl<<"Test train Done"<<std::endl<<std::endl;
+}
 
 int main(int argc, char *argv[])
 {
-    DeepLearning::Layer<DeepLearning::Sigmoid> layer1;
-    DeepLearning::Layer<DeepLearning::Sigmoid> layer2;
-    DeepLearning::Layer<DeepLearning::Naive>   layer3;
-
     std::cout<<"Hello Neural Network!"<<std::endl;
 
-    layer1.init(2,3);
-    layer2.init(3,2);
-    layer3.init(2,2);
-
-    double input[2] = {1.0, 0.5};
-
-    /// init layer1
+    /// test Train
     {
-        DeepLearning::Layer<DeepLearning::Naive> layer;
-        layer.init(2,3);
-        double input[2] = {1, 2};
-        rMat& weight = layer.getWeight();
-        weight[0][0] = 1;   weight[0][1] = 3;   weight[0][2] = 5;
-        weight[1][0] = 2;   weight[1][1] = 4;   weight[1][2] = 6;
-        layer.getBias()[0] = 0.0;
-        layer.getBias()[1] = 0.0;
-        layer.getBias()[2] = 0.0;
-        layer.feedForward(input,2);
-        rVec& output = layer.getOutput();
-        std::cout<<output[0]<<" "<<output[1]<<" "<<output[2]<<std::endl;
+        trainTest();
     }
 
-    /// test softMax
+    /// test multi layer train
     {
-        double a[3] = { 0.3, 2.9, 4.0 };
-        double b[3];
-        SoftMax::getSoftMax(3,a,b);
-        std::cout<<"---- softmax"<<std::endl;
-        std::cout<<b[0]<<" "<<b[1]<<" "<<b[2]<<std::endl;
-        std::cout<<"sum = "<<b[0] + b[1] + b[2]<<std::endl;
+        multiLayerTrain();
     }
 
-    /// init layer1
+    /// test matrix solve regression test
     {
-        rMat& weight = layer1.getWeight();
-        weight[0][0] = 0.1;   weight[0][1] = 0.3;   weight[0][2] = 0.5;
-        weight[1][0] = 0.2;   weight[1][1] = 0.4;   weight[1][2] = 0.6;
-        layer1.getBias()[0] = 0.1;
-        layer1.getBias()[1] = 0.2;
-        layer1.getBias()[2] = 0.3;
+        matrixSolveRegressionTest();
     }
-
-    /// init layer2
-    {
-        rMat& weight = layer2.getWeight();
-        weight[0][0] = 0.1;   weight[0][1] = 0.4;
-        weight[1][0] = 0.2;   weight[1][1] = 0.5;
-        weight[2][0] = 0.3;   weight[2][1] = 0.6;
-        layer2.getBias()[0] = 0.1;
-        layer2.getBias()[1] = 0.2;
-    }
-
-    /// init layer3
-    {
-        rMat& weight = layer3.getWeight();
-        weight[0][0] = 0.1;   weight[0][1] = 0.3;
-        weight[1][0] = 0.2;   weight[1][1] = 0.4;
-        layer3.getBias()[0] = 0.1;
-        layer3.getBias()[1] = 0.2;
-    }
-
-    /// test MSE
-    {
-        double t[10] = { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
-        double y0[10] = { 0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0};
-        double y1[10] = { 0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0};
-        std::cout<<"MSE = "<<MSE(10,y0,t)<<std::endl;
-        std::cout<<"MSE = "<<MSE(10,y1,t)<<std::endl;
-    }
-
-    /// test CEE
-    {
-        double t[10] = { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
-        double y0[10] = { 0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0};
-        double y1[10] = { 0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0};
-        std::cout<<"CEE = "<<CEE(10,y0,t)<<std::endl;
-        std::cout<<"CEE = "<<CEE(10,y1,t)<<std::endl;
-    }
-
-    /// test gradient_numerical
-    {
-        std::cout<<"TEST numerical Gradient"<<std::endl;
-        double input[2];;
-        double grad[2];
-        input[0] = 3.0;
-        input[1] = 4.0;
-        numerical_Gradient(function2,2,1,input,grad);
-        std::cout<<grad[0]<<" "<<grad[1]<<std::endl;
-        input[0] = 0.0;
-        input[1] = 2.0;
-        numerical_Gradient(function2,2,1,input,grad);
-        std::cout<<grad[0]<<" "<<grad[1]<<std::endl;
-        input[0] = 3.0;
-        input[1] = 0.0;
-        numerical_Gradient(function2,2,1,input,grad);
-        std::cout<<grad[0]<<" "<<grad[1]<<std::endl;
-    }
-
-    /// test Gradient Descent
-    {
-        double input[2] = {-3.0, 4.0};
-        gradientDescent(function2,0.1,100,2,1,input);
-        std::cout<<std::endl<<"Gradeint Descent Result = "<<input[0]<<" "<<input[1]<<std::endl<<std::endl;
-    }
-
-    /// test Gradient Descent
-    {
-        double input[2] = {-3.0, 4.0};
-        gradientDescent(function2,10.0,100,2,1,input);
-        std::cout<<std::endl<<"Gradeint Descent Result2 = "<<input[0]<<" "<<input[1]<<std::endl<<std::endl;
-    }
-
-    /// test Gradient Descent
-    {
-        double input[2] = {-3.0, 4.0};
-        gradientDescent(function2,1e-10,100,2,1,input);
-        std::cout<<std::endl<<"Gradeint Descent Result3 = "<<input[0]<<" "<<input[1]<<std::endl<<std::endl;
-    }
-
-    /// test a layer
-    {
-        double input[2] = { 0.6, 0.9 };
-        double target0[3] = { 1, 0, 0 };
-        double target1[3] = { 0, 1, 0 };
-        double target2[3] = { 0, 0, 1 };
-        std::cout<<"Test a Layer"<<std::endl;
-        DeepLearning::Layer<DeepLearning::Naive> layer;
-        layer.init(2,3);
-        rMat& weights = layer.getWeight();
-        std::cout<<weights[0][0]<<" "<<weights[0][1]<<" "<<weights[0][2]<<std::endl;
-        std::cout<<weights[1][0]<<" "<<weights[1][1]<<" "<<weights[1][2]<<std::endl;
-        layer.feedForward(input,2);
-        rVec& output = layer.getOutput();
-        std::cout<<"output = "<<output[0]<<" "<<output[1]<<" "<<output[2]<<std::endl;
-
-        rVec softmax(3,0);
-        SoftMax::getSoftMax(3,output.data(),softmax.data());
-        std::cout<<"softmax \n ==> "<<softmax[0]<<" "<<softmax[1]<<" "<<softmax[2]
-                 <<"\nSUM = "<<softmax[0] + softmax[1] + softmax[2]<<std::endl;
-        std::cout<<"cross entropy error\n ==> "<<CEE(3,softmax.data(),target0)<<" "
-                                           <<CEE(3,softmax.data(),target1)<<" "
-                                           <<CEE(3,softmax.data(),target2)<<std::endl;
-    }
-
-
-
-    layer1.feedForward(input,2);
-    layer2.feedForward(layer1.getOutput().data(),layer1.getOutput().size());
-    layer3.feedForward(layer2.getOutput().data(),layer2.getOutput().size());
-
-    std::cout<<"---- 3 layer neural network validation"<<std::endl;
-    std::cout<<"output = "<<layer3.getOutput()[0]<<" "<<layer3.getOutput()[1]<<std::endl;
 
     return 0;
 }
